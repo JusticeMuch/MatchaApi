002b97de1ee0f73ad4cfd9e3e78e6327676594df
nolanhendricks <pho3nix.01100001@gmail.com>
2020-07-09 16:50:55 +0200

    socket id added to array

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const Token = require('./models/token');
-const authToken = require('./middleware/verifyToken')
+const { authenticateToken } = require('./middleware/verifyToken');
 require('dotenv').config()
 const Pool = require('pg').Pool
 const profileRoute = require('./routes/profile');
 const notificationRoute = require('./routes/notifications');
+
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const authRoute = require('./routes/auth');
-const {db, pgp} = require('./db');
+const { db, pgp } = require('./db');
 const insertUserProfiles = require('./init');
 const QueryFile = pgp.QueryFile;
 
 
 const PORT = process.env.PORT || 8080;
 var corsOptions = {
-  origin: "http://localhost:8081"
+    origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
@@ -25,31 +29,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoute);
-app.use('/api/profile' ,authToken, profileRoute);
-app.use('/api', authToken, notificationRoute);
-// app.use('/api/profile', authToken, profileRoute); route with authentication
+app.use('/api/profile', authenticateToken, profileRoute);
+app.use('/api', authenticateToken, notificationRoute);
+// app.use('/api/profile', authenticateToken, profileRoute); route with authentication
 
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Matcha API" });
+    res.json({ message: "Welcome to Matcha API" });
 });
 
 
-mongoose
-.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(async () => {
-  await db.any(new QueryFile('matcha.pgsql'));
-  Token.collection.drop();
-  await insertUserProfiles();
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(async() => {
+    await db.any(new QueryFile('matcha.pgsql'));
+    Token.collection.drop();
+    await insertUserProfiles();
 
-  await app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });
-})
-.catch(err => {
-  console.log("Cannot connect to the database!", err);
-  process.exit();
+    await app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+    });
+}).catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
 });
+
+module.exports = {
+    io
+}
--- models/socket.js
+++ models/socket.js
@@ -1,5 +1,20 @@
+const { io } = require('index.js');
+const { authenticateSocketToken } = require('./../middleware/verifyToken');
 
+// add socket id and user id to array
+class Socket {
 
-class Socket{
-    
+    static connectedUsers = [];
+
+    newConnection = async(socket) => {
+        const id = await authenticateSocketToken(socket.handshake.query.token)._id;
+        await this.connectedUsers.push({ socketId: socket.id, userId: id });
+    }
+
+    socketConnection = async() => {
+        io.on('connection', async socket => {
+            await this.newConnection(socket);
+        })
+    }
+
 }
