67ddf98a0105bf7d6b754874f84b5af14afefc05
justicemuch <justinjamesqwerty@gmail.com>
2020-08-26 15:46:41 +0200

    Added getMessageById function

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -135,6 +135,8 @@ const getMessageById = async (req,res) => {
         }catch(error){
             return res.status(400).send({success : false, Error : {message : error.message}});
         }
+    }else{
+        res.status(400).send({success : false, Error : {message : "Fields (match_id, date) are empty or undefined "}});
     }
 }
 
